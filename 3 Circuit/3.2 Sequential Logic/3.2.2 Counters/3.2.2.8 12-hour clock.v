6ee0968150a72a5e42a7b84eb1f3bb5a501a0c5c
JJJayyy <60277173+JJJayyyy@users.noreply.github.com>
2021-03-06 18:55:25 -0800

    Update 3.2.2.8 12-hour clock.v

--- 3 Circuit/3.2 Sequential Logic/3.2.2 Counters/3.2.2.8 12-hour clock.v
+++ 3 Circuit/3.2 Sequential Logic/3.2.2 Counters/3.2.2.8 12-hour clock.v
@@ -6,62 +6,56 @@ module top_module(
     output [7:0] hh,
     output [7:0] mm,
     output [7:0] ss); 
-    
-    always@(posedge clk) begin
-        if (reset) begin
-            hh <= 8'h12;
-        	mm <= 0;
-        	ss <= 0;
-        	pm <= 0;
-        end
-        else begin
-            if (ena) begin
-                pm <= (hh == 8'h11 && mm == 8'h59 && ss == 8'h59)? ~pm : pm;
-                if (ss == 8'h59) begin // ss = 59
-                    ss <= 0;
-                    
-                    if (mm == 8'h59) begin // mm = 59
-                        mm <= 0;
-                        
-                        if (hh == 8'h12) begin
-                            hh <= 8'h01;
-                        end
-                        
-                        else begin
-                            if (hh[3:0] == 4'd9) begin
-                                hh[3:0] <= 0;
-                                hh[7:4] <= hh[7:4] + 4'd1;
-                            end
-                            else 
-                                hh[3:0] <= hh[3:0] + 4'd1;  
-                        end
-         
-                    end
-                    else begin
-                        if (mm[3:0] == 4'd9) begin
-                            mm[3:0] <= 0;
-                            mm[7:4] <= mm[7:4] + 4'd1;
-                        end
-                        else 
-                            mm[3:0] <= mm[3:0] + 4'd1; 
-                    end
-                    
-                    
-                end
-                else begin
-                    if (ss[3:0] == 4'd9) begin
-                        ss[3:0] <= 0;
-                        ss[7:4] <= ss[7:4] + 4'd1;
-                    end
-                    else 
-                        ss[3:0] <= ss[3:0] + 4'd1;
-                end
-            end   
-        end
-        
-        
+    always@(posedge clk)begin
+        if(reset)
+            pm = 0;
+        else
+            pm = ((hh[7:0]==8'h11)&&(ss[7:0]==8'h59)&&(mm[7:0]==8'h59))?~pm:pm;
     end
-    
-    
+        
+    wire inter1, inter2;
+    assign inter1 = (ss[7:0]==8'h59);
+    assign inter2 = (ss[7:0]==8'h59)&&(mm[7:0]==8'h59);
+    BCD60 s(clk, reset, ena, ss[7:0]);
+    BCD60 m(clk, reset, inter1, mm[7:0]);
+    BCD12 h(clk, reset, inter2, hh[7:0]);
+endmodule
 
+module BCD60 (input clk,input reset,input ena,output reg [7:0] Q);
+    always@(posedge clk) begin
+        if (reset) 
+            Q <= 8'h0;
+        else if (ena) begin
+                if (Q == 8'h59)
+                    Q <= 8'h0;
+                else begin
+                    if (Q[3:0] == 4'd9) begin
+            			Q[3:0] <= 0;
+            			Q[7:4] <= Q[7:4] + 4'd1;
+              		end
+              		else 
+                  		Q[3:0] <= Q[3:0] + 4'd1; 
+                end
+        end
+    end   
+endmodule
+
+module BCD12 (input clk,input reset,input ena,output reg [7:0] Q);
+    always@(posedge clk) begin
+        if (reset) 
+            Q <= 8'h12;
+        else if (ena)
+            begin
+                if (Q == 8'h12)
+                    Q <= 8'h1;
+                else begin
+                    if (Q[3:0] == 4'd9) begin
+            			Q[3:0] <= 0;
+            			Q[7:4] <= Q[7:4] + 4'd1;
+              		end
+              		else 
+                  		Q[3:0] <= Q[3:0] + 4'd1; 
+                end
+            end
+    end   
 endmodule
